0c460835fe66fa7a7ee2a8497f0ed4325956fe08
HmongDev <steveyangwork@gmail.com>
2024-10-30 12:39:50 -0500

    adjust width of experience card

--- components/shared/Experience.tsx
+++ components/shared/Experience.tsx
@@ -2,7 +2,7 @@ import ExperienceCard from './ExperienceCard';
 
 export default function Experience() {
 	return (
-		<main className="flex grow justify-center items-start p-5">
+		<main className="flex w-full justify-center items-center p-5">
 			<ExperienceCard />
 		</main>
 	);
--- components/shared/ExperienceCard.tsx
+++ components/shared/ExperienceCard.tsx
@@ -5,22 +5,14 @@ const ExperienceCard = () => {
 		<div className="w-full p-2">
 			{jobHistory.map((job) => {
 				return (
-					<div className="flex flex-col justify-between gap-3 my-5 bg-zinc-900 rounded-lg border-2 border-zinc-700">
+					<div className="flex flex-col w-3/5 justify-between gap-3 my-5 bg-zinc-900 rounded-lg border-zinc-700">
 						<div className="flex justify-between">
-							<h3 className="text-xl p-2">
-								{job.company}
-							</h3>
-							<h1 className="text-lg p-2">
-								{job.years}
-							</h1>
+							<h3 className="text-xl p-2">{job.company}</h3>
+							<h1 className="text-lg p-2">{job.years}</h1>
 						</div>
 						<div className="flex justify-between items-center p-2 bg-zinc-800 rounded-b">
-							<em className="text-md">
-								{job.position}
-							</em>
-							<h1 className="text-sm text-wrap p-2">
-								{job.location}
-							</h1>
+							<em className="text-md">{job.position}</em>
+							<h1 className="text-sm text-wrap p-2">{job.location}</h1>
 						</div>
 					</div>
 				);
